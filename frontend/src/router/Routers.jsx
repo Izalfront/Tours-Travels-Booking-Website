762663fa26e826d6dbaa644c6243157f4ac740d7
izalfront <rizalhajakubastarus@gmail.com>
2023-12-31 18:38:47 +0800

    landing page

---
+++ frontend/src/router/Routers.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+//import pages component from './pages
+import Home from './../pages/Home';
+import Login from './../pages/Login';
+import Register from './../pages/Register';
+import Tours from './../pages/Tours';
+import SearchResultList from './../pages/SearchResultList';
+import TourDetail from './../pages/TourDetail';
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="home" replace />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/tours" element={<Tours />} />
+      <Route path="/tours/:id" element={<TourDetail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/tours/search" element={<SearchResultList />} />
+    </Routes>
+  );
+};
+
+export default Routers;
