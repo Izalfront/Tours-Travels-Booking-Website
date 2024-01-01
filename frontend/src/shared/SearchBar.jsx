0566c955c3f775fea68ea138707110d0f281b016
izalfront <rizalhajakubastarus@gmail.com>
2024-01-01 19:50:12 +0800

    import search bar to Home

--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
-
+import './search-bar.css';
+import { Col, Form, FormGroup } from 'reactstrap';
 const SearchBar = () => {
-  return <div>searchBar</div>;
+  return (
+    <Col lg="2">
+      <div className="search__bar">
+        <Form className="d-flex align-items-center gap-4">
+          <FormGroup className="d-flex gap-3 form__group form__group-fast">
+            <span>
+              <i className="ri-map-pin-line"></i>
+            </span>
+            <div>
+              <h6>Location</h6>
+              <input type="text" name="location" id="location" placeholder="Where are you going ?" />
+            </div>
+          </FormGroup>
+          <FormGroup className="d-flex gap-3 form__group form__group-fast">
+            <span>
+              <i class="ri-map-pin-time-line"></i>
+            </span>
+            <div>
+              <h6>Distance</h6>
+              <input type="number" name="location" id="location" placeholder="Distance K/m" />
+            </div>
+          </FormGroup>
+          <FormGroup className="d-flex gap-3 form__group form__group-last">
+            <span>
+              <i class="ri-group-line"></i>
+            </span>
+            <div>
+              <h6>Max People</h6>
+              <input type="number" name="location" id="location" placeholder="0" />
+            </div>
+          </FormGroup>
+        </Form>
+      </div>
+    </Col>
+  );
 };
 
 export default SearchBar;
