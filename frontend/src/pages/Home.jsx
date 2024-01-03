02bc79221bf877701b523d4b419042542a800c06
izalfront <rizalhajakubastarus@gmail.com>
2024-01-03 21:43:05 +0800

    experience section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import heroImg from '../assets/images/hero-img01.jpg';
 import heroImg02 from '../assets/images/hero-img02.jpg';
 import heroVideo from '../assets/images/hero-video.mp4';
 import worldImg from '../assets/images/world.png';
+import experienceImg from '../assets/images/experience.png';
 import Subtitle from '../shared/Subtitle';
 import SearchBar from '../shared/SearchBar';
 import ServiceList from '../services/ServiceList';
@@ -97,11 +98,32 @@ const Home = () => {
                   In officiis, voluptatem sequi nihil culpa ducimus, ipsam quod quia
                 </p>
               </div>
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Successfull trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span>2k+</span>
+                  <h6>Regular client</h6>
+                </div>
+                <div className="counter__box">
+                  <span>15</span>
+                  <h6>Years experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="experience__img">
+                <img src={experienceImg} alt="" />
+              </div>
             </Col>
           </Row>
         </Container>
       </section>
       {/* ========== experience section end =========== */}
+      {/* ========== galery section start =========== */}
+      {/* ========== galery section end =========== */}
     </>
   );
 };
