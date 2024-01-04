53b2fb57b97393bb2b52f85bb07285e27e6824f2
izalfront <rizalhajakubastarus@gmail.com>
2024-01-04 10:16:41 +0800

    completed section footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,134 @@
 import React from 'react';
+import './footer.css';
+import logo from '../../assets/images/logo.png';
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
+const quick__links = [
+  {
+    path: '/home',
+    display: 'Home',
+  },
+  {
+    path: '/about',
+    display: 'About',
+  },
+  {
+    path: '/Tours',
+    display: 'Tours',
+  },
+];
+
+const quick__links2 = [
+  {
+    path: '/gallery',
+    display: 'Gallery',
+  },
+  {
+    path: '/login',
+    display: 'Login',
+  },
+  {
+    path: '/register',
+    display: 'Register',
+  },
+];
 function Footer() {
-  return <div>footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3">
+            <div className="logo">
+              <img src={logo} alt="" />
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, molestias!
+              <div className="social__links d-flex align-items-center gap-4">
+                <span>
+                  <Link to="#">
+                    <i class="ri-youtube-line"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-github-fill"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-instagram-line"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-linkedin-box-fill"></i>
+                  </Link>
+                </span>
+              </div>
+            </div>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__link-title">Discover</h5>
+            <ListGroup className="footer__quick-links">
+              {quick__links.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer_link-title">Quick Links</h5>
+            <ListGroup className="footer__quick-links">
+              {quick__links2.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer_link-title">Contact</h5>
+            <ListGroup className="footer__quick-links">
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6>
+                  <span className="mb-0 d-flex align-items-center gap-2">
+                    <i class="ri-map-pin-line"></i>
+                    Address:
+                  </span>
+                </h6>
+                <p className="mb-0">Banjarmasin, Kalimantan Selatan</p>
+              </ListGroupItem>
+
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6>
+                  <span className="mb-0 d-flex align-items-center gap-2">
+                    <i class="ri-mail-line"></i>
+                    Email:
+                  </span>
+                </h6>
+                <p className="mb-0">rizalhajakubastarus@gmail.com</p>
+              </ListGroupItem>
+
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6>
+                  <span className="mb-0 d-flex align-items-center gap-2">
+                    <i class="ri-phone-fill"></i>
+                    Phone:
+                  </span>
+                </h6>
+                <p className="mb-0">0812-1314-1334</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="12" className="text-center pt-5">
+            <p className="copyright">Copyright {year} Design and Develope by Izalfront. All rights reserverd. </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer;
