8182f00130e85d9e652be92a2543ed1d66fd55ee
izalfront <rizalhajakubastarus@gmail.com>
2024-01-04 15:40:09 +0800

    add page tour

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import CommonSection from '../shared/CommonSection';
+import '../styles/tour.css';
+import TourCard from './../shared/TourCard';
+import SearchBar from './../shared/SearchBar';
+import Newsletter from './../shared/Newsletter';
+import tourData from '../assets/data/tours';
+import { Container, Row, Col } from 'reactstrap';
 const Tours = () => {
-  return <div>Tours</div>;
+
+  const [pageCount, setPageCount] = useState(0)
+  
+  return (
+    <>
+      <CommonSection title={'All Tours'} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {tourData?.map((tour) => (
+              <Col lg="3" className="mb-4" key={tour.id}>
+                <TourCard tour={tour} />
+              </Col>
+
+              <Col lg='12'></Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default Tours;
