1608e9a26f46f389d0c785f142e17b6dfb6ff68b
izalfront <rizalhajakubastarus@gmail.com>
2024-01-08 17:50:56 +0800

    make login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Container, Row, Col, Form, FormGroup, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import '../styles/login.css';
+import loginImg from '../assets/images/login.png';
+import userIcon from '../assets/images/user.png';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="8" className="m-auto">
+            <div className="login__container d-flex justify-content-between">
+              <div className="login__img">
+                <img src={loginImg} alt="" />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
 export default Login;
