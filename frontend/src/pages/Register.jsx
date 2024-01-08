ea1c025f767d8349c1b45cdc309e0ebd2b045d94
izalfront <rizalhajakubastarus@gmail.com>
2024-01-08 22:22:06 +0800

    completed ui login & registser page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, FormGroup, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import '../styles/login.css';
+import registerImg from '../assets/images/register.png';
+import userIcon from '../assets/images/user.png';
 
-const Register = () => {
-  return <div>Register</div>;
-};
+function Register() {
+  const [credentials, setCredentials] = useState({
+    email: undefined,
+    password: undefined,
+    userName: undefined,
+  });
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="8" className="m-auto">
+            <div className="login__container d-flex justify-content-between">
+              <div className="login__img">
+                <img src={registerImg} alt="" />
+              </div>
+
+              <div className="login__form">
+                <div className="user">
+                  <img src={userIcon} alt="" />
+                </div>
+                <h2>Register</h2>
+
+                <Form onSubmit={handleClick}>
+                  <FormGroup>
+                    <input type="username" placeholder="Username" id="username" required onChange={handleChange} />
+                  </FormGroup>
+                  <FormGroup>
+                    <input type="email" placeholder="Email" id="email" required onChange={handleChange} />
+                  </FormGroup>
+                  <FormGroup>
+                    <input type="password" placeholder="Password" id="pasword" required onChange={handleChange} />
+                  </FormGroup>
+                  <Button className="btn secondary__btn auth__btn" type="submit">
+                    Creaate Account
+                  </Button>
+                </Form>
+                <p>
+                  Already have an account ? <Link to="/login">Login</Link>
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
 
 export default Register;
